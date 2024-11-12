6101510a33c43a9073d4745021bef0812fb50cd2
GiaHung1403 <giahung14031998@gmail.com>
2024-11-12 11:23:27 +0700

    -update

--- src/screens/detail-screen/item-list/index.tsx
+++ src/screens/detail-screen/item-list/index.tsx
@@ -13,21 +13,27 @@ export default function Item({title}: ItemProps) {
         marginRight: 8,
         marginBottom: 8,
       }}
-      onPress={() => setOnClick(!onClick)}>
+      onPress={() => setOnClick(!onClick)}
+    >
       <LinearGradient
-        colors={onClick ? ['#8A00FF', '#8A32A9'] : ['transparent']}
-        start={{x: 0, y: 0}}
-        end={{x: 0, y: 0.8}}
+        colors={
+          onClick ? ["#8A00FF", "#8A32A9"] : ["transparent", "transparent"]
+        }
+        start={{ x: 0, y: 0 }}
+        end={{ x: 0, y: 0.8 }}
         style={{
           borderWidth: 1,
           borderRadius: 8,
-          borderColor: `${'#FFFFFF'}12`,
+          borderColor: `${"#FFFFFF"}12`,
           flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
-          height: Dimensions.get('window').width / 5,
-        }}>
-        <Text style={{color: `${'#FFFFFF'}`, marginVertical: 10}}>{title}</Text>
+          alignItems: "center",
+          justifyContent: "center",
+          height: Dimensions.get("window").width / 5,
+        }}
+      >
+        <Text style={{ color: `${"#FFFFFF"}`, marginVertical: 10 }}>
+          {title}
+        </Text>
       </LinearGradient>
     </TouchableOpacity>
   );
